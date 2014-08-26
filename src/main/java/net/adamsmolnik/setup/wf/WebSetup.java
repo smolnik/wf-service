9d9fdc3cb947d7f616e9c19bd4bfd22ec5693c78
smolnik <smolnik@wp.pl>
2014-08-26 23:59:04 +0200

    Changes, changes... going forward

--- src/main/java/net/adamsmolnik/setup/wf/WebSetup.java
+++ src/main/java/net/adamsmolnik/setup/wf/WebSetup.java
@@ -7,10 +7,8 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 import net.adamsmolnik.setup.ServiceNameResolver;
 import net.adamsmolnik.util.Configuration;
-import net.adamsmolnik.util.ConfigurationKeys;
 import net.adamsmolnik.workflow.DataProcessingWorkflowImpl;
 import com.amazonaws.ClientConfiguration;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.simpleworkflow.AmazonSimpleWorkflow;
 import com.amazonaws.services.simpleworkflow.AmazonSimpleWorkflowClient;
 import com.amazonaws.services.simpleworkflow.flow.WorkflowWorker;
@@ -33,10 +31,7 @@ public class WebSetup implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ClientConfiguration config = new ClientConfiguration().withSocketTimeout(70 * 1000);
-        AmazonSimpleWorkflow service = conf.isSystemCredentialsExist() ? new AmazonSimpleWorkflowClient(new BasicAWSCredentials(
-                conf.getGlobalValue(ConfigurationKeys.ACCESS_KEY_ID.getKey()), conf.getGlobalValue(ConfigurationKeys.SECRET_KEY.getKey())), config)
-                : new AmazonSimpleWorkflowClient(config);
-
+        AmazonSimpleWorkflow service = new AmazonSimpleWorkflowClient(config);
         Map<String, String> confMap = conf.getServiceConfMap(snr.getServiceName());
         service.setEndpoint(confMap.get("swf.endpoint"));
         wfw = new WorkflowWorker(service, confMap.get("swf.domain"), confMap.get("swf.tasks"));
